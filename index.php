b04d611136f1f2a5097864894b480a72abefa5ce
alexandre <alexandre.rwada@gmail.com>
2018-08-16 02:38:22 -0300

    Classe par manipula arquivos de audio

--- index.php
+++ index.php
@@ -2,30 +2,7 @@
 
 require_once "vendor/autoload.php";
 
-$ffmpeg = FFMpeg\FFMpeg::create([
-    'ffmpeg.binaries'  => 'bin\ffmpeg.exe',
-    'ffprobe.binaries' => 'bin\ffprobe.exe'
-]);
+$con = new Audio\Manipulation();
+// $con->mp3toFlac('audio/x.mp3','audio/track.flac');
+echo $con->getDurationAudio('audio/x.mp3');
 
-$audio = $ffmpeg->open('audio/x.mp3');
-
-$format = new FFMpeg\Format\Audio\Flac();
-$format->on('progress', function ($audio, $format, $percentage) {
-    echo "$percentage % transcoded";
-});
-
-$format
-    ->setAudioChannels(1)
-    ->setAudioKiloBitrate(256);
-
-$audio->save($format, 'audio/track.flac');
-
-//  $translate = new Audio\Translate();
-
-//  $translate->config['languageCode']     = 'pt-BR';
-//  $translate->config['encoding']         = 'LINEAR16';
-//  $translate->config['sampleRateHertz']  = 16000;
-
-//  $x = $translate->TranslatorArchiveSync('audio/audio_example.flac');
-//  echo 'Textos: '.$x;
-//
---
+++ src/audio/Manipulation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Audio;
+
+use FFMpeg;
+use FFProbe;
+
+class Manipulation
+{
+
+    public $ffmpeg;
+    public $ffprobe;
+
+    public function __construct()
+    {
+
+        $this->ffmpeg = FFMpeg\FFMpeg::create([
+            'ffmpeg.binaries' => 'bin\ffmpeg.exe',
+            'ffprobe.binaries' => 'bin\ffprobe.exe',
+        ]);
+
+        $this->ffprobe = FFMpeg\FFProbe::create([
+            'ffmpeg.binaries' => 'bin\ffmpeg.exe',
+            'ffprobe.binaries' => 'bin\ffprobe.exe',
+        ]);
+
+    }
+
+    public function getDurationAudio($audioPatch) {
+        return $this->ffprobe->format($audioPatch)->get('duration');
+    }
+
+    public function mp3toFlac($mp3Patch, $flacPatch)
+    {
+        $audio = $this->ffmpeg->open($mp3Patch);
+        $format = new FFMpeg\Format\Audio\Flac();
+        // $format->on('progress', function ($audio, $format, $percentage) {
+        //     echo "$percentage % transcoded";
+        // });
+
+        $format
+            ->setAudioChannels(1)
+            ->setAudioKiloBitrate(256);
+
+        return $audio->save($format, $flacPatch);
+    }
+
+}
